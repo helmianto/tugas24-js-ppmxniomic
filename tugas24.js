b75a547097aab0edf16de01895856532832e6a70
Helmianto <helmianto15@gmail.com>
2021-06-30 16:49:04 +0700

    initial commit

---
+++ tugas24.js
@@ -0,0 +1,7 @@
+const numbers = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+console.log(`Sebelumnya : ${numbers}`);
+const ascending = numbers.sort();
+console.log(`Ascending : ${ascending}`);
+const descending = numbers.reverse();
+console.log(`Descending : ${descending}`);
+
